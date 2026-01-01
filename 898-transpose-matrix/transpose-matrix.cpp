4d99e489245de5383fa643786a7ab5dde3262a56
magicalSyntax <trixy1395@gmail.com>
2026-01-01 13:52:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (33.99%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        vector<vector<int>> ans(col, vector<int>(row));
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[i].size(); j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
