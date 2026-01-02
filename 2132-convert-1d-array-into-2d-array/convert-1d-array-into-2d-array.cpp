08630f6644a5663eb923f709171e68fefc01cf17
magicalSyntax <trixy1395@gmail.com>
2026-01-02 17:22:56 +0530

    Time: 20 ms (20.35%) | Memory: 89.9 MB (60.78%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        vector<vector<int>> ans(m, vector<int>(n));
+        
         if(m*n != original.size()){
             return {};
         }
         int l = 0;
+        vector<vector<int>> ans(m, vector<int>(n));
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 ans[i][j] = original[l];
