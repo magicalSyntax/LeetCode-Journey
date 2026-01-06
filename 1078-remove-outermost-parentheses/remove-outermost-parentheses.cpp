a5d51d464f6fb9ab545a1076e9c746891ace559f
magicalSyntax <trixy1395@gmail.com>
2026-01-06 23:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (96.63%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans;
+        int depth = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                if(depth > 0) ans += s[i];
+                depth++;
+            }
+            else if(s[i] == ')'){ 
+                depth--;
+                if(depth > 0) ans += s[i];
+            }
+        }
+        return ans;
+    }
+};
