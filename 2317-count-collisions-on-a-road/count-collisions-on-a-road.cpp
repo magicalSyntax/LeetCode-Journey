429019056ac5683b1169cfd940d50c020c8911c6
magicalSyntax <trixy1395@gmail.com>
2026-01-14 11:22:28 +0530

    Time: 12 ms (43.65%) | Memory: 19.2 MB (71.95%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int countCollisions(string dir) {
-        int n = dir.size();
+    int countCollisions(string directions) {
+        int n = directions.size();
         if(n == 1) return 0;
         int i = 0, j = n-1;
-        while(i < n && dir[i] == 'L') i++;
-        while(j >= 0 && dir[j] == 'R') j--;
+        while(i < n && directions[i] == 'L') i++;
+        while(j >= 0 && directions[j] == 'R') j--;
         int count = 0;
         for(int k = i; k <= j; k++){
-            if(dir[k] != 'S') count++;
+            if(directions[k] != 'S') count++;
         }
         return count;
     }
