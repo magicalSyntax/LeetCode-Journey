a50b134d5f96bd37e2729cedbd0f63d9a58d833d
magicalSyntax <trixy1395@gmail.com>
2026-01-15 14:34:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (6.94%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,18 +1,47 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        if(needle.empty()) return -1;
+
         int m = haystack.size();
         int n = needle.size();
-        for(int i = 0; i < m-n+1; i++){
-            if(haystack[i] == needle[0]){
-                int j = 1;
-                bool flag = true;
-                while(j < n){
-                    if(haystack[i+j] != needle[j++]) flag = false;
+        
+        //LSP
+        vector<int> lps(n, 0);
+        for(int i = 1, len = 0; i < n;){
+            if(needle[i] == needle[len]){
+                len++;
+                lps[i] = len;
+                i++;
+            }else{
+                if(len!=0) {
+                    len = lps[len - 1];
+                }else {
+                    lps[i] = 0;
+                    i++;
                 }
-                if(flag == true) return i;
             }
         }
+
+        //KMP Search
+        int i = 0, j = 0;
+        while(i < m){
+            if(haystack[i] == needle[j]){
+                i++;
+                j++;
+            }
+
+            if(j == n){
+                return i - j;
+            }
+            else if(i < m && haystack[i] != needle[j]){
+                if(j != 0)
+                    j = lps[j-1];
+                else
+                    i++;
+            }
+        }
+
         return -1;
     }
 };
